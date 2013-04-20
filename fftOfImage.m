ef81758f26bcc041c3f5de965a187ea771757dac
Adam Szczombrowski <adam.szczombrowski@gmail.com>
2013-04-20 12:48:01 +0200

    Dodany negatyw, wybieranie 1 ćwiartki obrazu i wyświetlanie fft 1 wiersza

--- fftOfImage.m
+++ fftOfImage.m
@@ -1,11 +1,12 @@
 function [] = fftOfImage(nameOfImage, labelOne, labelTwo)
 
-imageA = imread(nameOfImage,'jpg');
+imageA = imread(nameOfImage,'jpg');                 % wczytywanie obrazu
 
 % Converting RGB image to binary image
-imageA = rgb2gray(imageA);
+imageA = rgb2gray(imageA);                          
 levelA = graythresh(imageA);
 imageA = im2bw(imageA, levelA);
+imageA = ~imageA;                                   %robienie negatywu
 
 % Showing images
 figure, imshow(imageA)
@@ -14,10 +15,14 @@ title(labelOne)
 % FFT algorithm
 fftA = fft2(double(imageA));
 fftA = fftshift(fftA);
-fftA = abs(fftA);
-fftA = log(fftA + 1);
-fftA = mat2gray(fftA);
+fftA = abs(fftA);                                   %get the magnitude
+fftB = log(fftA + 1);
+fftA = mat2gray(fftB);
+[m,n] = size(fftA);
+fftA = fftA(1:m/2,n/2:n);                           %wybieranie 1 æwiartki
 figure, imshow(fftA, []); % Display the result
 title(labelTwo)
+
+figure, stem(fftA(1,:))                             % fft jednego wiersza
 
 end
