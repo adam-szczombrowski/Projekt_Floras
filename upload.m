9762c6d00971053f63dcd727f0cb9fb62d0f9bc5
AleksanderGrzyb <aleksander.grzyb@gmail.com>
2013-05-05 14:12:49 +0200

    - Dodane slidery - Poprawa UI - Transformata w kolumnach i rzędach - Dodatkowe błędy poprawione

--- upload.m
+++ upload.m
@@ -22,7 +22,7 @@ function varargout = upload(varargin)
 
 % Edit the above text to modify the response to help upload
 
-% Last Modified by GUIDE v2.5 21-Apr-2013 19:54:53
+% Last Modified by GUIDE v2.5 04-May-2013 18:25:11
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -73,40 +73,47 @@ function varargout = upload_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
 
 
-% --- Executes on button press in pushbutton_upload.
+
 function pushbutton_upload_Callback(hObject, eventdata, handles)
-[filename pathname] = uigetfile('*.jpg;*.bmp','Select bmp file');
-ab = strcat(pathname,filename)
-axes(handles.axes2);
-imshow(ab);
-imageA = imread(ab);
-
-imageA = rgb2gray(imageA);                          
-levelA = graythresh(imageA);
-imageA = im2bw(imageA, levelA);
-imageA = ~imageA;          
-
-fftA = fft2(double(imageA));
-fftA = fftshift(fftA);
-fftA = abs(fftA);                                   %get the magnitude
-fftB = log(fftA + 1);
-fftA = mat2gray(fftB);
-[m,n] = size(fftA);
-fftA = fftA(1:m/2,n/2:n); 
-
-axes(handles.axes3);
-imshow(fftA, []); % Display the result
-
-
-handles.pushbutton_upload = fftA;
-guidata(hObject,handles) 
+    [filename pathname] = uigetfile('*.jpg;*.bmp','Select bmp file');
+    if isequal(filename, 0) || isequal(pathname, 0)  
+    else
+        ab = strcat(pathname,filename);
+        axes(handles.axes2);
+        imshow(ab);
+        imageA = imread(ab);
+        imageA = rgb2gray(imageA);                          
+        levelA = graythresh(imageA);
+        imageA = im2bw(imageA, levelA);
+        imageA = ~imageA;          
+        fftA = fft2(double(imageA));
+        fftA = fftshift(fftA);
+        fftA = abs(fftA);
+        fftB = log(fftA + 1);
+        fftA = mat2gray(fftB);
+        [m,n] = size(fftA);
+        fftA = fftA(1:m/2,n/2:n); 
+        axes(handles.axes3);
+        imshow(fftA, []);
+        handles.pushbutton_upload = fftA;
+        [m,n] = size(fftA);
+        pos = get(handles.axes3, 'position');
+        width = pos(4) / m * n;
+        set(handles.text4, 'visible', 'on');
+        set(handles.text5, 'visible', 'on');
+        set(handles.slider2, 'visible', 'on');
+        set(handles.slider2, 'position', [pos(1) + pos(3) * 0.5 - width * 0.5, pos(2) - 16, width, 16]);
+        set(handles.slider3, 'visible', 'on');
+        set(handles.slider3, 'position', [pos(1) + pos(3) * 0.5 + width * 0.5, pos(2), 16, pos(4)]);
+        guidata(hObject,handles)   
+    end
 
 %plot+scrollowanie+poziom+pion+mniejsza rozdzielczosc(4lub8 razy128/256)
 %badania + wnioski = raport
-%zamiast stem mamy uzyc plota, trzeba dodaæ inne okno do kolumn
-%transformaty, wybieranie wiersza/kolumny - trzeba rozkminiæ jakieœ
+%zamiast stem mamy uzyc plota, trzeba doda? inne okno do kolumn
+%transformaty, wybieranie wiersza/kolumny - trzeba rozkmini? jakie?
 %scrollowanie zamiast wybierania w edit text, zmniejszyc
-%rozdzielczosc(wyswietlac co któras próbke)
+%rozdzielczosc(wyswietlac co kt?ras pr?bke)
 
 % hObject    handle to pushbutton_upload (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -170,3 +177,88 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
+
+% --- Executes on mouse press over figure background, over a disabled or
+% --- inactive control, or over an axes background.
+function figure1_WindowButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on slider movement.
+function slider2_Callback(hObject, eventdata, handles)
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of
+%        slider
+    axes(handles.axes4);
+    [m, n] = size(handles.pushbutton_upload);
+    value = get(hObject, 'Value');
+    currentColumn = uint32(value * m);
+    if currentColumn > 0
+        currentCoumnString = int2str(currentColumn);
+        newString = ['Column: ' currentCoumnString];
+        set(handles.text3, 'String', newString);
+        set(handles.text3, 'visible', 'on');
+        stem(handles.pushbutton_upload(currentColumn,:));
+    else
+        set(handles.text3, 'String', 'Column: 1');
+        set(handles.text3, 'visible', 'on');
+        plot(handles.pushbutton_upload(1,:));
+    end
+    
+    
+
+
+% --- Executes during object creation, after setting all properties.
+function slider2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes on slider movement.
+function slider3_Callback(hObject, eventdata, handles)
+% hObject    handle to slider3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+    axes(handles.axes5);
+    [m, n] = size(handles.pushbutton_upload);
+    value = get(hObject, 'Value');
+    currentRow = uint32(value * n);
+    if currentRow > 0 
+        currentRowString = int2str(n - currentRow + 1);
+        newString = ['Row: ' currentRowString];
+        set(handles.text2, 'String', newString);
+        set(handles.text2, 'visible', 'on');
+        stem(handles.pushbutton_upload(:,currentRow));
+    else
+        newString = ['Row: ' int2str(n)];
+        set(handles.text2, 'String', newString);
+        set(handles.text2, 'visible', 'on');
+        plot(handles.pushbutton_upload(:,1));
+    end
+
+
+% --- Executes during object creation, after setting all properties.
+function slider3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
