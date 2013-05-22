8f439767401ed2b68d68f291cdcfee90047e8f0e
AleksanderGrzyb <aleksander.grzyb@gmail.com>
2013-05-22 14:49:21 +0200

    - Gotowa dokumentacja - Poprawki w kodzie: poprawione slidery, zmiana nazwy programu, zmiany kosmetyczne - Nowe zdjęcia znaczków oraz wygenerowane ich transformaty.

--- ProjektPrzejsciowy.m
+++ ProjektPrzejsciowy.m
@@ -1,26 +1,26 @@
-function varargout = upload(varargin)
-% UPLOAD M-file for upload.fig
-%      UPLOAD, by itself, creates a new UPLOAD or raises the existing
+function varargout = ProjektPrzejsciowy(varargin)
+% ProjektPrzejsciowy M-file for ProjektPrzejsciowy.fig
+%      ProjektPrzejsciowy, by itself, creates a new ProjektPrzejsciowy or raises the existing
 %      singleton*.
 %
-%      H = UPLOAD returns the handle to a new UPLOAD or the handle to
+%      H = ProjektPrzejsciowy returns the handle to a new ProjektPrzejsciowy or the handle to
 %      the existing singleton*.
 %
-%      UPLOAD('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in UPLOAD.M with the given input arguments.
+%      ProjektPrzejsciowy('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in ProjektPrzejsciowy.M with the given input arguments.
 %
-%      UPLOAD('Property','Value',...) creates a new UPLOAD or raises the
+%      ProjektPrzejsciowy('Property','Value',...) creates a new ProjektPrzejsciowy or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before upload_OpeningFcn gets called.  An
+%      applied to the GUI before ProjektPrzejsciowy_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to upload_OpeningFcn via varargin.
+%      stop.  All inputs are passed to ProjektPrzejsciowy_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help upload
+% Edit the above text to modify the response to help ProjektPrzejsciowy
 
 % Last Modified by GUIDE v2.5 04-May-2013 18:25:11
 
@@ -28,8 +28,8 @@ function varargout = upload(varargin)
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @upload_OpeningFcn, ...
-                   'gui_OutputFcn',  @upload_OutputFcn, ...
+                   'gui_OpeningFcn', @ProjektPrzejsciowy_OpeningFcn, ...
+                   'gui_OutputFcn',  @ProjektPrzejsciowy_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,26 +44,26 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before upload is made visible.
-function upload_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before ProjektPrzejsciowy is made visible.
+function ProjektPrzejsciowy_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to upload (see VARARGIN)
+% varargin   command line arguments to ProjektPrzejsciowy (see VARARGIN)
 
-% Choose default command line output for upload
+% Choose default command line output for ProjektPrzejsciowy
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes upload wait for user response (see UIRESUME)
+% UIWAIT makes ProjektPrzejsciowy wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = upload_OutputFcn(hObject, eventdata, handles) 
+function varargout = ProjektPrzejsciowy_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -74,11 +74,11 @@ varargout{1} = handles.output;
 
 
 
-function pushbutton_upload_Callback(hObject, eventdata, handles)
+function pushbutton_ProjektPrzejsciowy_Callback(hObject, eventdata, handles)
     [filename pathname] = uigetfile('*.jpg;*.bmp','Select bmp file');
     if isequal(filename, 0) || isequal(pathname, 0)  
     else
-        ab = strcat(pathname,filename);
+        ab = strcat(pathname, filename);
         axes(handles.axes2);
         imshow(ab);
         imageA = imread(ab);
@@ -95,16 +95,30 @@ function pushbutton_upload_Callback(hObject, eventdata, handles)
         fftA = fftA(1:m/2,n/2:n); 
         axes(handles.axes3);
         imshow(fftA, []);
-        handles.pushbutton_upload = fftA(1:4:end,1:4:end);
+        handles.pushbutton_ProjektPrzejsciowy = fftA(1:4:end,1:4:end);
+        
+        
         [m,n] = size(fftA);
         pos = get(handles.axes3, 'position');
         width = pos(4) / m * n;
+        
+        [maxX, maxY] = size(handles.pushbutton_ProjektPrzejsciowy);
+        
         set(handles.text4, 'visible', 'on');
         set(handles.text5, 'visible', 'on');
         set(handles.slider2, 'visible', 'on');
         set(handles.slider2, 'position', [pos(1) + pos(3) * 0.5 - width * 0.5, pos(2) - 16, width, 16]);
+        set(handles.slider2, 'Value', 1);
+        set(handles.slider2, 'Min', 1);
+        set(handles.slider2, 'Max', maxY);
+        set(handles.slider2, 'SliderStep', [1, 1] / (maxY - 1));
+        
         set(handles.slider3, 'visible', 'on');
         set(handles.slider3, 'position', [pos(1) + pos(3) * 0.5 + width * 0.5, pos(2), 16, pos(4)]);
+        set(handles.slider3, 'Value', maxX);
+        set(handles.slider3, 'Min', 1);
+        set(handles.slider3, 'Max', maxX);
+        set(handles.slider3, 'SliderStep', [1, 1] / (maxX - 1));
         guidata(hObject,handles)   
     end
 
@@ -115,7 +129,7 @@ function pushbutton_upload_Callback(hObject, eventdata, handles)
 %scrollowanie zamiast wybierania w edit text, zmniejszyc
 %rozdzielczosc(wyswietlac co kt?ras pr?bke)
 
-% hObject    handle to pushbutton_upload (see GCBO)
+% hObject    handle to pushbutton_ProjektPrzejsciowy (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
@@ -157,9 +171,9 @@ function edit2_Callback(hObject, eventdata, handles)
 
 x = str2double(get(hObject, 'String'));
 axes(handles.axes4);
-[m,n] = size(handles.pushbutton_upload);
+[m,n] = size(handles.pushbutton_ProjektPrzejsciowy);
 if m >= x
-    stem(handles.pushbutton_upload(x,:));
+    stem(handles.pushbutton_ProjektPrzejsciowy(x,:));
 else
     error('wiersz poza zakresem');
 end
@@ -195,24 +209,15 @@ function slider2_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'Value') returns position of slider
 %        get(hObject,'Min') and get(hObject,'Max') to determine range of
 %        slider
-    axes(handles.axes4);
-    [m, n] = size(handles.pushbutton_upload);
-    value = get(hObject, 'Value');
-    currentColumn = uint32(value * m);
-    if currentColumn > 0
-        currentCoumnString = int2str(currentColumn);
-        newString = ['Column: ' currentCoumnString];
-        set(handles.text3, 'String', newString);
-        set(handles.text3, 'visible', 'on');
-        plot(handles.pushbutton_upload(currentColumn,:));
-    else
-        set(handles.text3, 'String', 'Column: 1');
-        set(handles.text3, 'visible', 'on');
-        plot(handles.pushbutton_upload(1,:));
-    end
-    
-    
 
+    axes(handles.axes4);
+    [m, n] = size(handles.pushbutton_ProjektPrzejsciowy);
+    currentColumn = floor(get(hObject, 'Value'));
+    currentColumnString = int2str(currentColumn);
+    newString = ['Column: ' currentColumnString];
+    set(handles.text3, 'String', newString);
+    set(handles.text3, 'visible', 'on');
+    plot(handles.pushbutton_ProjektPrzejsciowy( : , currentColumn));
 
 % --- Executes during object creation, after setting all properties.
 function slider2_CreateFcn(hObject, eventdata, handles)
@@ -232,24 +237,14 @@ function slider3_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'Value') returns position of slider
-%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
     axes(handles.axes5);
-    [m, n] = size(handles.pushbutton_upload);
-    value = get(hObject, 'Value');
-    currentRow = uint32(value * n);
-    if currentRow > 0 
-        currentRowString = int2str(n - currentRow + 1);
-        newString = ['Row: ' currentRowString];
-        set(handles.text2, 'String', newString);
-        set(handles.text2, 'visible', 'on');
-        plot(handles.pushbutton_upload(:,currentRow));
-    else
-        newString = ['Row: ' int2str(n)];
-        set(handles.text2, 'String', newString);
-        set(handles.text2, 'visible', 'on');
-        plot(handles.pushbutton_upload(:,1));
-    end
+    [m, n] = size(handles.pushbutton_ProjektPrzejsciowy);
+    currentRow = floor(get(hObject, 'Value'));
+    currentRowString = int2str(m - currentRow + 1);
+    newString = ['Row: ' currentRowString];
+    set(handles.text2, 'String', newString);
+    set(handles.text2, 'visible', 'on');
+    plot(handles.pushbutton_ProjektPrzejsciowy(m - currentRow + 1, :));
 
 
 % --- Executes during object creation, after setting all properties.
